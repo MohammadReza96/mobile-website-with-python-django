f0cf36222762cb8d018c44fca8a3eb4b53baf21f
MohammadReza96 <mohammadrz.yazdani.96@gmail.com>
2023-04-02 02:20:12 +0330

    update search app

--- dj_shop/apps/search/views.py
+++ dj_shop/apps/search/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.views import View
 from django.db.models import Q,Avg,Max,Min
 from apps.products.models import Product
+from apps.blogs.models import Blog
 
+#----------------------------------------------------- search view (base on product & blogs)
 class SearchResultView(View):
     def get(self,request,*args,**kwargs):
         search_value=request.GET.get('search')
         product_list=Product.objects.filter(Q(product_name__icontains=search_value))
-        return render(request,'search_app/search_app.html',{'product':product_list})
+        blog_list=Blog.objects.filter(Q(blog_title__icontains=search_value))
+        return render(request,'search_app/search_app.html',{'product':product_list,'blog_list':blog_list})
         
